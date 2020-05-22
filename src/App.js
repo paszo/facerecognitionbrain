2fff779670eeeeec821b123254ce78b193a879e7
paszo <paszo2008@gmail.com>
2020-05-22 21:59:27 +0200

    Added face detection box

--- src/App.js
+++ src/App.js
@@ -33,7 +33,27 @@ class App extends Component{
         this.state = {
             input: '',
             imageUrl: '',
+            box: {},
         }
+    }
+
+    calculateFaceLocation = (data) => {
+            const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+            const image = document.getElementById('inputimage');
+            const width = Number(image.width);
+            const height = Number(image.height);
+            return {
+                leftCol: clarifaiFace.left_col * width,
+                topRow: clarifaiFace.top_row * height,
+                rightCol: width - (clarifaiFace.right_col * width),
+                bottomRow: height - (clarifaiFace.bottom_row * height)
+
+            }
+    }
+
+    displayFaceBox = (box) => {
+        console.log(box);
+        this.setState({box: box})
     }
 
     onInputChange = (event) => {
@@ -43,16 +63,12 @@ class App extends Component{
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
         console.log('click');
-        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-            .then(
-                function(response) {
-                    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-                    //do something with response
-                },
-                function(err) {
-                    // there was an error
-                }
-            );
+        app.models
+            .predict(
+                Clarifai.FACE_DETECT_MODEL,
+                this.state.input)
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+            .catch(err => console.log(err));
     }
 
     render()
@@ -67,7 +83,7 @@ class App extends Component{
                  onInputChange={this.onInputChange}
                  onButtonSubmit={this.onButtonSubmit}
              />
-            <FaceRecognition imageUrl={this.state.imageUrl}/>
+            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
         </div>
     );
     }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({box, imageUrl}) => {
     return (
         <div className='center ma'>
-            <div className='center ma'>
-                <img alt='' src={imageUrl} width='500px' height='auto'/>
+            <div className='absolute ma'>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+                <div className='bounding-box' style={{top: box.topRow , right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
+                </div>
             </div>
         </div>
     );
