fa6c309eab07e050c63e3bbe11334ef43e128bbc
paszo <paszo2008@gmail.com>
2020-05-23 08:48:05 +0200

    Updates

---
+++ precache-manifest.1ec817cf10167c5116efa27b56e8d321.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b397b500bc6c242d7bfa61ec92dfa281",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "7846957a4e74d889a122",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8dbd7ba890946f567192",
+    "url": "/facerecognitionbrain/static/css/main.bb50fa88.chunk.css"
+  },
+  {
+    "revision": "7846957a4e74d889a122",
+    "url": "/facerecognitionbrain/static/js/2.8d9c9852.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.8d9c9852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dbd7ba890946f567192",
+    "url": "/facerecognitionbrain/static/js/main.dd9d7864.chunk.js"
+  },
+  {
+    "revision": "8b0a425d16ee78af64e6",
+    "url": "/facerecognitionbrain/static/js/runtime-main.dbbcf49d.js"
+  }
+]);
